a68612a6ea4339cbb21cea3b11e7fb2101f955ec
Simranpal Singh <Simranpal989@gmail.com>
2026-01-07 14:03:30 +0530

    added fix for mobile app

--- server.ts
+++ server.ts
@@ -53,7 +53,7 @@ app.get("/", (req, res) => {
   res.send("Car Parts Backend API is running 🚀");
 });
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+const PORT = Number(process.env.PORT) || 4000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
